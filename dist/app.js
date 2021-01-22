92214cbb6ce4fce758f1bece6310dfda787e7045
unknown <ianchen6501@gmail.com>
2021-01-23 01:11:19 +0800

    header 改為異步載入

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("yo");
+var obj = {
+  name: "ian"
+};
+var name = obj.name.name;
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 //babel, cleanify, uglify, sass
 const gulp = require("gulp")
 const babel = require('gulp-babel')
+var sass = require('gulp-sass');
 
-gulp.task('babel', async() => {
-  await gugulp.src('src/app.js')
+gulp.task('default', (done) => {
+  gulp.src('./src/app.js')
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('./dist'))
+  return done()
 })
 
-function defaultTask(cb) {
-  
-  cb();
-}
-
-exports.default = defaultTask
-exports.babel = babel
+gulp.task('sass', function () {
+  return gulp.src('./src/.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./static/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/*.scss', ['sass']);
+});
